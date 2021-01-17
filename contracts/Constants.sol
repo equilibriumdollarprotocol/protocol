80e5a3dc6b77fd55a1abf47eb58da376e3b21dee
Equilibrium Dollar Protocol <77573641+equilibriumdollarprotocol@users.noreply.github.com>
2021-01-17 20:04:40 +0700

    Add files via upload

---
+++ contracts/Constants.sol
@@ -0,0 +1,162 @@
+/*
+    Copyright 2021 Equilibrium Dollar Protocol, based on the works of the Empty Set Squad
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "./external/Decimal.sol";
+
+library Constants {
+    /* Chain */
+    uint256 private constant CHAIN_ID = 56; // BSC
+
+    /* Bootstrapping */
+    uint256 private constant BOOTSTRAPPING_PERIOD = 360; // 360 epochs
+    uint256 private constant BOOTSTRAPPING_PRICE = 122e16; // 1.22 BUSD
+
+    /* Oracle */
+    address private constant BUSD = address(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
+    uint256 private constant ORACLE_RESERVE_MINIMUM = 1e10; // 10,000 BUSD
+
+    /* Bonding */
+    uint256 private constant INITIAL_STAKE_MULTIPLE = 1e6; // 100 EQD -> 100M EQD-S
+
+    /* Epoch */
+    struct EpochStrategy {
+        uint256 offset;
+        uint256 start;
+        uint256 period;
+    }
+
+    uint256 private constant EPOCH_OFFSET = 0;
+    uint256 private constant EPOCH_START = 1610895600;
+    uint256 private constant EPOCH_PERIOD = 3600;
+
+    /* Governance */
+    uint256 private constant GOVERNANCE_PERIOD = 20;
+    uint256 private constant GOVERNANCE_QUORUM = 33e16; // 33%
+    uint256 private constant GOVERNANCE_SUPER_MAJORITY = 66e16; // 66%
+    uint256 private constant GOVERNANCE_EMERGENCY_DELAY = 6; // 6 epochs
+
+    /* DAO */
+    uint256 private constant ADVANCE_INCENTIVE = 2e18; // 2 EQD
+    uint256 private constant DAO_EXIT_LOCKUP_EPOCHS = 24; // 24 epochs fluid
+
+    /* Pool */
+    uint256 private constant POOL_EXIT_LOCKUP_EPOCHS = 10; // 10 epochs fluid
+
+    /* Market */
+    uint256 private constant COUPON_EXPIRATION = 720;
+    uint256 private constant DEBT_RATIO_CAP = 35e16; // 35%
+    uint256 private constant INITIAL_COUPON_REDEMPTION_PENALTY = 50e16; // 50%
+    uint256 private constant COUPON_REDEMPTION_PENALTY_DECAY = 3600; // 1 hour
+
+    /* Regulator */
+    uint256 private constant SUPPLY_CHANGE_DIVISOR = 10e18; // 10
+    uint256 private constant SUPPLY_CHANGE_LIMIT = 3e16; // 3%
+    uint256 private constant ORACLE_POOL_RATIO = 40; // 40%
+
+    /**
+     * Getters
+     */
+    function getUsdcAddress() internal pure returns (address) {
+        return BUSD;
+    }
+
+    function getOracleReserveMinimum() internal pure returns (uint256) {
+        return ORACLE_RESERVE_MINIMUM;
+    }
+
+    function getEpochStrategy() internal pure returns (EpochStrategy memory) {
+        return EpochStrategy({
+            offset: EPOCH_OFFSET,
+            start: EPOCH_START,
+            period: EPOCH_PERIOD
+        });
+    }
+
+    function getInitialStakeMultiple() internal pure returns (uint256) {
+        return INITIAL_STAKE_MULTIPLE;
+    }
+
+    function getBootstrappingPeriod() internal pure returns (uint256) {
+        return BOOTSTRAPPING_PERIOD;
+    }
+
+    function getBootstrappingPrice() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: BOOTSTRAPPING_PRICE});
+    }
+
+    function getGovernancePeriod() internal pure returns (uint256) {
+        return GOVERNANCE_PERIOD;
+    }
+
+    function getGovernanceQuorum() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: GOVERNANCE_QUORUM});
+    }
+
+    function getGovernanceSuperMajority() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: GOVERNANCE_SUPER_MAJORITY});
+    }
+
+    function getGovernanceEmergencyDelay() internal pure returns (uint256) {
+        return GOVERNANCE_EMERGENCY_DELAY;
+    }
+
+    function getAdvanceIncentive() internal pure returns (uint256) {
+        return ADVANCE_INCENTIVE;
+    }
+
+    function getDAOExitLockupEpochs() internal pure returns (uint256) {
+        return DAO_EXIT_LOCKUP_EPOCHS;
+    }
+
+    function getPoolExitLockupEpochs() internal pure returns (uint256) {
+        return POOL_EXIT_LOCKUP_EPOCHS;
+    }
+
+    function getCouponExpiration() internal pure returns (uint256) {
+        return COUPON_EXPIRATION;
+    }
+
+    function getDebtRatioCap() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: DEBT_RATIO_CAP});
+    }
+    
+    function getInitialCouponRedemptionPenalty() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: INITIAL_COUPON_REDEMPTION_PENALTY});
+    }
+
+    function getCouponRedemptionPenaltyDecay() internal pure returns (uint256) {
+        return COUPON_REDEMPTION_PENALTY_DECAY;
+    }
+
+    function getSupplyChangeLimit() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: SUPPLY_CHANGE_LIMIT});
+    }
+
+    function getSupplyChangeDivisor() internal pure returns (Decimal.D256 memory) {
+        return Decimal.D256({value: SUPPLY_CHANGE_DIVISOR});
+    }
+
+    function getOraclePoolRatio() internal pure returns (uint256) {
+        return ORACLE_POOL_RATIO;
+    }
+
+    function getChainId() internal pure returns (uint256) {
+        return CHAIN_ID;
+    }
+}
---
+++ contracts/dao/Implementation.sol
@@ -0,0 +1,56 @@
+/*
+    Copyright 2021 Equilibrium Dollar Protocol, based on the works of the Empty Set Squad and Dynamic Dollar Devs
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+pragma solidity ^0.5.17;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./Market.sol";
+import "./Regulator.sol";
+import "./Bonding.sol";
+import "./Govern.sol";
+import "../Constants.sol";
+
+contract Implementation is State, Bonding, Market, Regulator, Govern {
+    using SafeMath for uint256;
+
+    event Advance(uint256 indexed epoch, uint256 block, uint256 timestamp);
+    event Incentivization(address indexed account, uint256 amount);
+
+    function initialize() initializer public {
+        // committer reward:
+        mintToAccount(msg.sender, 200e18); // 200 EQD to Deployer
+        // contributor  rewards:
+        mintToAccount(0x80cD8EAdFD7e7829c087EE614901820807c72633, 200e18); // 150 EQD to Airdrop
+        mintToAccount(0x407D2C4AAc97511347D0678F293A1d50aD69cE11, 100e18); // 150 EQD to Team
+    }
+
+    function advance() external incentivized {
+        Bonding.step();
+        Regulator.step();
+        Market.step();
+
+        emit Advance(epoch(), block.number, block.timestamp);
+    }
+
+    modifier incentivized {
+        // Mint advance reward to sender
+        uint256 incentive = Constants.getAdvanceIncentive();
+        mintToAccount(msg.sender, incentive);
+        emit Incentivization(msg.sender, incentive);
+        _;
+    }
+}
